1cf2a20d25cedfae1bd466125d96f44c05e1e60b
Igor Kozolovski <igor@beyondbasics.co>
2022-03-13 23:19:47 +0100

    create mongodb init shell script to create dedicated user for the finki_forum database that can use env vars and apply the required changes in the docker compose file, remove the previously defined mongodb init js file

---
+++ init-mongo.sh
@@ -0,0 +1,89 @@
+mongo -- "MONGO_DBNAME" << EOF
+	db.createUser(
+		{
+			user: '$MONGO_USERNAME', 
+			pwd: '$MONGO_PASSWORD', 
+			roles: [
+				{
+					role: 'readWrite',
+					db: '$MONGO_DBNAME'
+				}
+			]
+		}
+	);
+
+	db.users.insertOne(
+		{
+			"_id": ObjectId(),
+			"avatar_image": "",
+			"birth_day": "",
+			"birth_month": "",
+			"birth_year": "",
+			"email": "admin@gmail.com",
+			"firstname": "Igor",
+			"group_type": "Администратор",
+			"join_date": new Date(),
+			"last_activity": new Date(),
+			"lastname": "Kozolovski",
+			"likes_num": 0,
+			"likes_posts_ids": [],
+			"password": "f865b53623b121fd34ee5426c792e5c33af8c227",
+			"posts_num": 0,
+			"username": "admin"
+		}
+	);
+
+	db.forums.insertOne(
+		{
+			"_id" : ObjectId("600c453276c74d191c8b4567"),
+			"title" : "Добредојдовте на ФИНКИ",
+			"description" : "Добредојдовте на неофицијалниот форум на Факултетот за Информатички Науки и Компјутерско Инженерство",
+			"display_order" : NumberLong(1),
+			"date" : ISODate("2021-01-23T15:48:02.854Z"),
+			"forum_moderators_num" : NumberLong(0),
+			"subforums" : [ 
+				{
+					"id" : ObjectId("600c454876c74d1c1c8b4567"),
+					"title" : "Информации за факултетот",
+					"description" : "Општи информации за факултетот",
+					"display_order" : NumberLong(1),
+					"date" : ISODate("2021-01-23T15:48:24.782Z"),
+					"subforum_moderators_num" : NumberLong(0),
+					"topics_num" : NumberLong(0),
+					"posts_num" : NumberLong(0)
+				}, 
+				{
+					"id" : ObjectId("600c459e76c74da21b8b4567"),
+					"title" : "Додипломски студии",
+					"description" : "Информации за додипломски студии",
+					"display_order" : NumberLong(2),
+					"date" : ISODate("2021-01-23T15:49:50.103Z"),
+					"subforum_moderators_num" : NumberLong(0),
+					"topics_num" : NumberLong(0),
+					"posts_num" : NumberLong(0)
+				}, 
+				{
+					"id" : ObjectId("600c45ad76c74d9e1b8b4567"),
+					"title" : "Магистерски студии",
+					"description" : "Информации за магистерски студии",
+					"display_order" : NumberLong(3),
+					"date" : ISODate("2021-01-23T15:50:05.673Z"),
+					"subforum_moderators_num" : NumberLong(0),
+					"topics_num" : NumberLong(0),
+					"posts_num" : NumberLong(0)
+				}, 
+				{
+					"id" : ObjectId("600c45bb76c74d151c8b4567"),
+					"title" : "Докторски студии",
+					"description" : "Информации за докторски студии",
+					"display_order" : NumberLong(4),
+					"date" : ISODate("2021-01-23T15:50:19.638Z"),
+					"subforum_moderators_num" : NumberLong(0),
+					"topics_num" : NumberLong(0),
+					"posts_num" : NumberLong(0)
+				}
+			]
+		}
+	);
+	
+EOF
